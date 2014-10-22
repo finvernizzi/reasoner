b1bd6a2ba493f796f5777cac0a91acc252156900
finvernizzi <fabrizio.invernizzi@telecomitalia.it>
2014-10-22 17:13:21 +0200

    gateways can have mmultiple adresses

--- reasoner.js
+++ reasoner.js
@@ -11,14 +11,18 @@ var gw1 = new network.Gateway({
     IPs:["192.168.123.1/26" , "192.168.123.130/30" ,"192.168.123.65/26"],
     hostName : "GW1"
 });
-//var link1 = new network.Link(gw1 , gw2 , "Test link");
-/*
-var net1 = new network.Network({
+var mplaneGw = new network.Gateway({
+    IPs:["192.168.123.129/30" , "163.162.170.197/27"],
+    hostName : "mplaneGw"
+});
+var link1 = new network.Link(gw1 , mplaneGw , "Internet link");
+
+var serviceNet = new network.Network({
     name : "Test net",
     subnet: "192.168.123.0/24",
     gateways : [gw1],
     links: [link1]
 });
 
-*/
+
 console.log(gw1);
