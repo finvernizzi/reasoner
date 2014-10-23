774cc2e04eb1f928278a67865fd015a321c28855
finvernizzi <fabrizio.invernizzi@telecomitalia.it>
2014-10-23 09:19:08 +0200

    First work on probes

--- reasoner.js
+++ reasoner.js
@@ -1,9 +1,14 @@
 /**
  * Created by fabrizio.invernizzi@telecomitalia.it on 21/10/14.
+ * Reasoner imports net definitions from json an build a graph representation (graphlib) of it with some simple assumption.
+ * No routing info are considered!
+ * We can also add nodes adding /32 as subnet.
  */
 
 var NETWORK_DEFINITION = "./demoNet.json";
 var RTT_CAPABILITY = "mean.rtt";
+// Network we want to do reachability checks toward
+var DESTINATION_NET = "internet";
 
 var network=require("./network.js")
     ,_ = require("lodash")
@@ -30,6 +35,7 @@ if (!netDef){
     console.error("Error reading from network definition file");
     process.exit();
 }
+info("Network definitions loaded");
 
 // Import networks/gateways to generate a graph representation
 var netGraph = new graphLib.Graph({ directed: false});
@@ -43,23 +49,20 @@ _.each(netDef.networks , function(net , netName){
     else
         __subnetIndex[ip.cidr(net.subnet)] = netName; //INDEXED
 });
+info("...Subnet nodes edges created");
 // add edges for /32 subnets. These nets should have only 1 gw
 _.each(netDef.networks , function(net , netName){
     var netInfo = ip.cidrSubnet(net.subnet)
     if (netInfo.subnetMaskLength == 32){
-        console.log("--------------------");
-        console.log(net);
         var gw = netDef.gateways[net.gateways[0]]; // Default gw of the host
-        console.log(gw);
         for (var i=0; i<gw.IPs.length;i++) {
             if (netInfo.networkAddress != ip.cidr(gw.IPs[i])){
-                console.log(__subnetIndex[ip.cidr(gw.IPs[i])] + " <-> " + net.gateways[0])
                 netGraph.setEdge(netName, __subnetIndex[ip.cidr(gw.IPs[i])] , net.gateways[0]);
             }
         }
     }
 });
-
+info("...Host nodes linked");
 // Graph edges from gateways
 // Edges are identified using subnets
 _.each(netDef.gateways , function(gw , gwName){
@@ -71,20 +74,28 @@ _.each(netDef.gateways , function(gw , gwName){
         }
     }
 });
-
+info("...Edges created");
 // Spanning tree , Dijstra tree from internet. All edge has the same weigth
 // TODO: use measure to change node weihtght?
-var spanTree = graphLib.alg.prim(netGraph, function(){return 1;});
-var dijkstraTree = graphLib.alg.dijkstra(netGraph, "internet" , function(){return 1;});
+//var spanTree = graphLib.alg.prim(netGraph, function(){return 1;});
+var dijkstraTree = graphLib.alg.dijkstra(netGraph, DESTINATION_NET , function(){return 1;});
+info("Graph created");
+info("..."+netGraph.nodeCount()+" networks");
+info("..."+netGraph.edgeCount()+" links");
 
+console.log(netGraph.sinks())
 
-console.log(dijkstraTree)
 /*
 { serviceNet: { distance: 2, predecessor: 'internetUplink' },
     internetUplink: { distance: 1, predecessor: 'internet' },
     internet: { distance: 0 },
     userAccess: { distance: 2, predecessor: 'internetUplink' } }
 */
+
+
+
+
+
 
 function motd(){
     console.log();
@@ -108,4 +119,8 @@ function motd(){
     console.log("    An Intelligent Measurement Plane for Future \n         Network and Application Management");
     console.log();
     console.log();
+}
+
+function info(msg){
+    console.log("..."+msg);
 }
