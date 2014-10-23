081b609d1e9c9aee838ed031a67b8e2f1b89025a
finvernizzi <fabrizio.invernizzi@telecomitalia.it>
2014-10-23 16:16:04 +0200

    debug

--- reasoner.js
+++ reasoner.js
@@ -212,7 +212,7 @@ function ipBelongsToNet(IPadd){
     _.each(_.keys(__subnetIndex) , function(netId , index){
         var netInfo = ip.cidrSubnet(getNetworkSubnet(netId));
         //console.log(netInfo)
-        //console.log(IPadd + " - " + netInfo.firstAddress+" - "+netInfo.lastAddress)
+        console.log(IPadd + " - " + netInfo.firstAddress+" - "+netInfo.lastAddress)
         if ((ip.toLong(IPadd) >= ip.toLong(netInfo.firstAddress)) && (ip.toLong(IPadd) <= ip.toLong(netInfo.lastAddress))){
             //console.log("OK")
             return netId;
