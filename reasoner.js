03cf2e18e401035b469862d3370bb9bb7190f1bf
finvernizzi <fabrizio.invernizzi@telecomitalia.it>
2014-10-24 16:33:08 +0200

    CheckStatus for retriving results and triggering analysis

--- reasoner.js
+++ reasoner.js
@@ -267,6 +267,7 @@ function checkStatus(){
                         }
                     }else {
                         var result = mplane.from_dict(body);
+                        console.log(result);
                     }
                 });
         });
